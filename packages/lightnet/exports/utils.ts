ab6c332620d4cd05e44448856f1ba133b25fa850
Simon <lgtnt@protonmail.com>
2025-03-19 07:54:39 +0100

    simplify path utilities

--- packages/lightnet/exports/utils.ts
+++ packages/lightnet/exports/utils.ts
@@ -1,2 +1,2 @@
-export * from "../src/utils/paths"
+export { searchPagePath, detailsPagePath } from "../src/utils/paths"
 export { verifySchema, verifySchemaAsync } from "../src/utils/verify-schema"
