12cdbce0632dfad78301f8247d1bd266a0b1e26a
Simon <lgtnt@protonmail.com>
2025-03-26 13:45:32 +0100

    Rename and export details page components

--- packages/lightnet/exports/details-page.ts
+++ packages/lightnet/exports/details-page.ts
@@ -1,1 +1,4 @@
-export { default as MediaCollections } from "../src/pages/details-page/components/MediaCollections.astro"
+export { default as MediaCollectionsSection } from "../src/pages/details-page/components/MediaCollectionsSection.astro"
+export { default as DescriptionSection } from "../src/pages/details-page/components/DescriptionSection.astro"
+export { default as ContentSection } from "../src/pages/details-page/components/ContentSection.astro"
+export { default as MoreDetailsSection } from "../src/pages/details-page/components/more-details/MoreDetailsSection.astro"
