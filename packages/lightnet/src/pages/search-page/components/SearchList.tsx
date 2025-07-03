7258e0b68077915f04f6e5b32cdada0c5681eec4
Simon <lgtnt@protonmail.com>
2025-07-03 06:52:10 +0200

    fix scroll restoration for long lists

--- packages/lightnet/src/pages/search-page/components/SearchList.tsx
+++ packages/lightnet/src/pages/search-page/components/SearchList.tsx
@@ -31,7 +31,7 @@ export default function SearchList({
   mediaItemsTotal,
 }: Props) {
   const listRef = useRef<HTMLDivElement | null>(null)
-  const [rowHeight, setRowHeight] = useState(208)
+  const [rowHeight, setRowHeight] = useState(256)
   const { results, isLoading } = useSearch({
     currentLocale,
     categories,
