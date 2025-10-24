e25a2d5413e30e7b0eec978a118912a3c394d2a2
Simon <lgtnt@protonmail.com>
2025-10-24 16:24:33 +0200

    make error color brighter

--- packages/lightnet/src/admin/components/form/FieldErrors.tsx
+++ packages/lightnet/src/admin/components/form/FieldErrors.tsx
@@ -10,7 +10,7 @@ export const FieldErrors = ({ meta }: FieldErrorsProps) => {
   return (
     <ul className="my-2 flex flex-col gap-1" role="alert">
       {meta.errors.map((error) => (
-        <li className="text-sm text-rose-900" key={error.code}>
+        <li className="text-sm text-rose-800" key={error.code}>
           {error.message}
         </li>
       ))}
