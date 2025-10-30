d4f0ba3c7d0ba8157146160df35f9e1f85ff1d65
Simon <lgtnt@protonmail.com>
2025-10-30 17:07:46 +0100

    Add invalid date error message

--- packages/lightnet/src/admin/types/media-item.ts
+++ packages/lightnet/src/admin/types/media-item.ts
@@ -1,11 +1,12 @@
 import { z } from "astro/zod"
 
 const NON_EMPTY_STRING = "ln.admin.errors.non-empty-string"
+const INVALID_DATE = "ln.admin.errors.invalid-date"
 
 export const mediaItemSchema = z.object({
   commonId: z.string().nonempty(NON_EMPTY_STRING),
   title: z.string().nonempty(NON_EMPTY_STRING),
-  dateCreated: z.string().date()
+  dateCreated: z.string().date(INVALID_DATE)
 })
 
 export type MediaItem = z.infer<typeof mediaItemSchema>
