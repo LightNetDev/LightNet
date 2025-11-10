4373ba12d0eaeadfa60d75e60143ef584b134dae
Simon <lgtnt@protonmail.com>
2025-11-10 17:10:15 +0100

    restructure e2e tests

--- packages/lightnet/__e2e__/admin.spec.ts
+++ packages/lightnet/__e2e__/admin.spec.ts
@@ -4,110 +4,112 @@ import { lightnetTest } from "./test-utils"
 
 const test = lightnetTest("./fixtures/basics/")
 
-test("Should not show `Edit` button on details page by default.", async ({
-  page,
-  startLightnet,
-}) => {
-  await startLightnet()
+test.describe("Edit button on details page", () => {
+  test("Should not show `Edit` button on details page by default.", async ({
+    page,
+    startLightnet,
+  }) => {
+    await startLightnet()
 
-  await page.getByRole("link", { name: "Faithful Freestyle" }).click()
-  await expect(
-    page.getByRole("heading", { name: "Faithful Freestyle" }),
-  ).toBeVisible()
+    await page.getByRole("link", { name: "Faithful Freestyle" }).click()
+    await expect(
+      page.getByRole("heading", { name: "Faithful Freestyle" }),
+    ).toBeVisible()
 
-  const editButton = page.locator("#edit-btn")
-  await expect(editButton).toBeHidden()
-})
+    const editButton = page.locator("#edit-btn")
+    await expect(editButton).toBeHidden()
+  })
 
-test("Should show `Edit` button on book details page after visiting `/en/admin/` path.", async ({
-  page,
-  startLightnet,
-}) => {
-  const ln = await startLightnet()
+  test("Should show `Edit` button on book details page after visiting `/en/admin/` path.", async ({
+    page,
+    startLightnet,
+  }) => {
+    const ln = await startLightnet()
 
-  await page.goto(ln.resolveURL("/en/admin/"))
-  await expect(
-    page.getByText("Admin features are enabled now.", { exact: true }),
-  ).toBeVisible()
+    await page.goto(ln.resolveURL("/en/admin/"))
+    await expect(
+      page.getByText("Admin features are enabled now.", { exact: true }),
+    ).toBeVisible()
 
-  await page.goto(ln.resolveURL("/en/media/faithful-freestyle--en"))
-  await expect(
-    page.getByRole("heading", { name: "Faithful Freestyle" }),
-  ).toBeVisible()
+    await page.goto(ln.resolveURL("/en/media/faithful-freestyle--en"))
+    await expect(
+      page.getByRole("heading", { name: "Faithful Freestyle" }),
+    ).toBeVisible()
 
-  const editButton = page.locator("#edit-btn")
-  await expect(editButton).toBeVisible()
-  await expect(editButton).toHaveAttribute(
-    "href",
-    "/en/admin/media/faithful-freestyle--en",
-  )
-})
+    const editButton = page.locator("#edit-btn")
+    await expect(editButton).toBeVisible()
+    await expect(editButton).toHaveAttribute(
+      "href",
+      "/en/admin/media/faithful-freestyle--en",
+    )
+  })
 
-test("Should show `Edit` button on video details page after visiting `/en/admin/` path.", async ({
-  page,
-  startLightnet,
-}) => {
-  const ln = await startLightnet()
+  test("Should show `Edit` button on video details page after visiting `/en/admin/` path.", async ({
+    page,
+    startLightnet,
+  }) => {
+    const ln = await startLightnet()
 
-  await page.goto(ln.resolveURL("/en/admin/"))
-  await expect(
-    page.getByText("Admin features are enabled now.", { exact: true }),
-  ).toBeVisible()
+    await page.goto(ln.resolveURL("/en/admin/"))
+    await expect(
+      page.getByText("Admin features are enabled now.", { exact: true }),
+    ).toBeVisible()
 
-  await page.goto(ln.resolveURL("/en/media/how-to-kickflip--de"))
-  await expect(
-    page.getByRole("heading", { name: "Kickflip Anleitung" }),
-  ).toBeVisible()
+    await page.goto(ln.resolveURL("/en/media/how-to-kickflip--de"))
+    await expect(
+      page.getByRole("heading", { name: "Kickflip Anleitung" }),
+    ).toBeVisible()
 
-  const editButton = page.locator("#edit-btn")
-  await expect(editButton).toBeVisible()
-  await expect(editButton).toHaveAttribute(
-    "href",
-    "/en/admin/media/how-to-kickflip--de",
-  )
-})
+    const editButton = page.locator("#edit-btn")
+    await expect(editButton).toBeVisible()
+    await expect(editButton).toHaveAttribute(
+      "href",
+      "/en/admin/media/how-to-kickflip--de",
+    )
+  })
 
-test("Should show `Edit` button on audio details page after visiting `/en/admin/` path.", async ({
-  page,
-  startLightnet,
-}) => {
-  const ln = await startLightnet()
+  test("Should show `Edit` button on audio details page after visiting `/en/admin/` path.", async ({
+    page,
+    startLightnet,
+  }) => {
+    const ln = await startLightnet()
 
-  await page.goto(ln.resolveURL("/en/admin/"))
-  await expect(
-    page.getByText("Admin features are enabled now.", { exact: true }),
-  ).toBeVisible()
+    await page.goto(ln.resolveURL("/en/admin/"))
+    await expect(
+      page.getByText("Admin features are enabled now.", { exact: true }),
+    ).toBeVisible()
 
-  await page.goto(ln.resolveURL("/en/media/skate-sounds--en"))
-  await expect(
-    page.getByRole("heading", { name: "Skate Sounds" }),
-  ).toBeVisible()
+    await page.goto(ln.resolveURL("/en/media/skate-sounds--en"))
+    await expect(
+      page.getByRole("heading", { name: "Skate Sounds" }),
+    ).toBeVisible()
 
-  const editButton = page.locator("#edit-btn")
-  await expect(editButton).toBeVisible()
-  await expect(editButton).toHaveAttribute(
-    "href",
-    "/en/admin/media/skate-sounds--en",
-  )
-})
+    const editButton = page.locator("#edit-btn")
+    await expect(editButton).toBeVisible()
+    await expect(editButton).toHaveAttribute(
+      "href",
+      "/en/admin/media/skate-sounds--en",
+    )
+  })
 
-test("Edit button on details page should navigate to media item edit page", async ({
-  page,
-  startLightnet,
-}) => {
-  const ln = await startLightnet()
+  test("Edit button on details page should navigate to media item edit page", async ({
+    page,
+    startLightnet,
+  }) => {
+    const ln = await startLightnet()
 
-  await page.goto(ln.resolveURL("/en/admin/"))
-  await page.goto(ln.resolveURL("/en/media/faithful-freestyle--en"))
+    await page.goto(ln.resolveURL("/en/admin/"))
+    await page.goto(ln.resolveURL("/en/media/faithful-freestyle--en"))
 
-  const editButton = page.locator("#edit-btn")
-  await expect(editButton).toBeVisible()
+    const editButton = page.locator("#edit-btn")
+    await expect(editButton).toBeVisible()
 
-  await editButton.click()
-  await expect(page).toHaveURL(
-    ln.resolveURL("/en/admin/media/faithful-freestyle--en"),
-  )
-  await expect(
-    page.getByText("Edit media item", { exact: false }),
-  ).toBeVisible()
+    await editButton.click()
+    await expect(page).toHaveURL(
+      ln.resolveURL("/en/admin/media/faithful-freestyle--en"),
+    )
+    await expect(
+      page.getByText("Edit media item", { exact: false }),
+    ).toBeVisible()
+  })
 })
