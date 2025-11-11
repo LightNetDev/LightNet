9f4c914a1ebccf804e626b9b40741b56772fdf2d
Simon <lgtnt@protonmail.com>
2025-11-11 09:45:19 +0100

    add shared array component

---
+++ packages/lightnet/src/admin/components/form/DynamicArray.tsx
@@ -0,0 +1,77 @@
+import type { ReactNode } from "react"
+import {
+  type ArrayPath,
+  type Control,
+  type FieldValues,
+  useFieldArray,
+  type UseFieldArrayAppend,
+} from "react-hook-form"
+
+import Icon from "../../../components/Icon"
+import { useI18n } from "../../../i18n/react/useI18n"
+import ErrorMessage from "./atoms/ErrorMessage"
+import Hint from "./atoms/Hint"
+import Legend from "./atoms/Legend"
+
+export default function DynamicArray<TFieldValues extends FieldValues>({
+  control,
+  name,
+  label,
+  hint,
+  renderElement,
+  addButton,
+}: {
+  name: ArrayPath<TFieldValues>
+  label: string
+  hint?: string
+  control: Control<TFieldValues>
+  renderElement: (index: number) => ReactNode
+  addButton: {
+    label: string
+    onClick: (
+      append: UseFieldArrayAppend<TFieldValues, ArrayPath<TFieldValues>>,
+      elementIndex: number,
+    ) => void
+  }
+}) {
+  const { fields, append, remove } = useFieldArray({
+    name,
+    control,
+  })
+  const { t } = useI18n()
+  return (
+    <fieldset key={name}>
+      <Legend label={label} />
+      <div className="flex w-full flex-col divide-y divide-gray-300 rounded-lg border border-gray-300">
+        {fields.map((field, index) => (
+          <div className="p-2" key={field.id}>
+            <div className="flex w-full items-center gap-2">
+              <div className="flex grow flex-col">{renderElement(index)}</div>
+              <button
+                className="flex items-center p-2 text-gray-600 hover:text-gray-900"
+                type="button"
+                onClick={() => remove(index)}
+              >
+                <Icon
+                  className="mdi--remove"
+                  ariaLabel={t("ln.admin.remove")}
+                />
+              </button>
+            </div>
+          </div>
+        ))}
+        <button
+          type="button"
+          className="p-4 text-sm font-bold text-gray-600 hover:bg-gray-200"
+          onClick={() => {
+            addButton.onClick(append, fields.length)
+          }}
+        >
+          {t(addButton.label)}
+        </button>
+      </div>
+      <ErrorMessage name={name} control={control} />
+      <Hint hint={hint} />
+    </fieldset>
+  )
+}
--- packages/lightnet/src/admin/pages/media/fields/Authors.tsx
+++ packages/lightnet/src/admin/pages/media/fields/Authors.tsx
@@ -1,58 +1,29 @@
-import { type Control, useFieldArray } from "react-hook-form"
+import { type Control } from "react-hook-form"
 
-import Icon from "../../../../components/Icon"
-import { useI18n } from "../../../../i18n/react/useI18n"
 import ErrorMessage from "../../../components/form/atoms/ErrorMessage"
-import Hint from "../../../components/form/atoms/Hint"
-import Legend from "../../../components/form/atoms/Legend"
+import DynamicArray from "../../../components/form/DynamicArray"
 import type { MediaItem } from "../../../types/media-item"
 
 export default function Authors({ control }: { control: Control<MediaItem> }) {
-  const { fields, append, remove } = useFieldArray({
-    name: "authors",
-    control,
-  })
-  const { t } = useI18n()
   return (
-    <fieldset key="authors">
-      <Legend label="ln.admin.authors" />
-      <div className="flex w-full flex-col divide-y divide-gray-300 rounded-lg border border-gray-300">
-        {fields.map((author, index) => (
-          <div className="p-2" key={author.id}>
-            <div className="flex w-full items-center gap-2">
-              <input
-                className="dy-input dy-input-sm grow"
-                {...control.register(`authors.${index}.value`)}
-              />
-              <button
-                className="flex items-center p-2 text-gray-600 hover:text-gray-900"
-                type="button"
-                onClick={() => remove(index)}
-              >
-                <Icon
-                  className="mdi--remove"
-                  ariaLabel={t("ln.admin.remove")}
-                />
-              </button>
-            </div>
-            <ErrorMessage name={`authors.${index}.value`} control={control} />
-          </div>
-        ))}
-        <button
-          type="button"
-          className="p-4 text-sm font-bold text-gray-600 hover:bg-gray-200"
-          onClick={() => {
-            append(
-              { value: "" },
-              { focusName: `authors.${fields.length}.value` },
-            )
-          }}
-        >
-          {t("ln.admin.add-author")}
-        </button>
-      </div>
-      <ErrorMessage name="authors" control={control} />
-      <Hint />
-    </fieldset>
+    <DynamicArray
+      control={control}
+      name="authors"
+      label="ln.admin.authors"
+      renderElement={(index) => (
+        <>
+          <input
+            className="dy-input dy-input-sm grow"
+            {...control.register(`authors.${index}.value`)}
+          />
+          <ErrorMessage name={`authors.${index}.value`} control={control} />
+        </>
+      )}
+      addButton={{
+        label: "ln.admin.add-author",
+        onClick: (append, index) =>
+          append({ value: "" }, { focusName: `authors.${index}.value` }),
+      }}
+    />
   )
 }
