bf0c4b19ad6b543dcf7675a815d1837bb9d54e52
Simon <lgtnt@protonmail.com>
2025-12-15 06:16:53 +0100

    remove daisyui from search input

--- packages/lightnet/src/pages/search-page/components/Select.tsx
+++ packages/lightnet/src/pages/search-page/components/Select.tsx
@@ -12,21 +12,23 @@ export default function Select({
   options,
 }: Props) {
   return (
-    <label className="dy-form-control">
-      <span className="my-2 block text-xs font-bold uppercase text-gray-500">
+    <label>
+      <span className="mb-1 mt-2 block text-xs font-bold uppercase text-gray-600">
         {label}
       </span>
-      <select
-        className="dy-select dy-select-bordered w-full rounded-xl sm:dy-select-sm"
-        value={initialValue}
-        onChange={(e) => valueChange(e.currentTarget.value)}
-      >
-        {options.map(({ id, labelText }) => (
-          <option key={id} value={id}>
-            {labelText}
-          </option>
-        ))}
-      </select>
+      <div className="rounded-2xl border border-gray-300 bg-white px-4 py-3 shadow-sm outline outline-2 outline-offset-2 outline-transparent transition-all ease-in-out focus-within:outline-gray-300 sm:p-2">
+        <select
+          className="w-full bg-white focus:outline-none sm:text-sm"
+          value={initialValue}
+          onChange={(e) => valueChange(e.currentTarget.value)}
+        >
+          {options.map(({ id, labelText }) => (
+            <option key={id} value={id}>
+              {labelText}
+            </option>
+          ))}
+        </select>
+      </div>
     </label>
   )
 }
